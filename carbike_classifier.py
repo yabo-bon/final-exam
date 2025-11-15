cfb2496c10fb662dee222e7b3bb8c0aa8c6f58bb
yabo <151602554+yabo-bon@users.noreply.github.com>
2025-11-15 20:07:33 +0800

    Update carbike_classifier.py

--- carbike_classifier.py
+++ carbike_classifier.py
@@ -9,7 +9,7 @@ st.markdown("""
     <style>
     /* ---- Background Image ---- */
     .stApp {
-        background-image: url('https://example.com/your-background.jpg');
+        background-image: url('https://github.com/yabo-bon/final-exam/blob/main/background.jpgg');
         background-size: cover; 
         background-position: center;
         background-repeat: no-repeat;
